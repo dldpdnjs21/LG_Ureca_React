01505a5638383cdd0993f6fd46fa8f6179c2a9b1
Yewon_Lee <65122491+dldpdnjs21@users.noreply.github.com>
2024-08-12 14:55:53 +0900

    Hook_useCounter

---
+++ day0812/hook/src/myFile/Accommodate.jsx
@@ -0,0 +1,11 @@
+//Acommodate(입장하는 인원을 수용하는 컴포넌트)
+
+const MAX_CAPACITY = 10; //최대수용인원
+
+export default function Accommodate(props){
+    return(
+        <div>
+            
+        </div>
+    );
+}
