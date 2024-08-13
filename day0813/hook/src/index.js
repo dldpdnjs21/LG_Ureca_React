9e5cffeb66621b3dd8ff322ecc0bd610743af3ac
Yewon_Lee <65122491+dldpdnjs21@users.noreply.github.com>
2024-08-13 15:42:39 +0900

    Hook_event

--- day0813/hook/src/index.js
+++ day0813/hook/src/index.js
@@ -3,11 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import MyToggle from './myFile2/MyToggle';
+import MyFunctionToggle from './myFile2/MyFunctionToggle';
+import ConfirmButton from './myFile2/ConfirmButton';
+import ConfirmButton2 from './myFile2/ConfirmButton2';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    {/* <MyToggle /> */}
+    {/* <MyFunctionToggle /> */}
+    {/* <ConfirmButton /> */}
+    <ConfirmButton2 />
   </React.StrictMode>
 );
 
