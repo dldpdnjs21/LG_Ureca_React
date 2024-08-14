d568e0a4115fe4011f4c40cbca3ee3b5c83c0e9f
Yewon_Lee <65122491+dldpdnjs21@users.noreply.github.com>
2024-08-14 14:22:14 +0900

    간단한 로그인 상태 구현

---
+++ day0814/src/MyFile/MyNum.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import Greeting from "./Greeting";
+
+function MyNum(props) {
+    //난수 발생 
+    const  [com_num, setCom_num] = useState(Math.floor( Math.random()*100 + 1 ));  //1~100
+
+    //사용자가 입력한 데이터를 (상태)관리
+    const [user_num, setUser_num] = useState("");
+    const [result, setResult] = useState("");
+
+    function checkGuess(){
+        console.log("com_num=", com_num, ", user_num=", user_num);
+        //값비교 ==결과==> 낮춰주세요 / 높여주세요
+        //기준값 ==> 난수를 발생
+        if(user_num > com_num){//사용자 입력값이 높을때
+            setResult(`[${user_num}] DOWN`);
+       }else if(user_num < com_num){//사용자 입력값이 낮을때
+            setResult(`[${user_num}] UP`);
+       }else{//정답
+            setResult("BINGO!");
+       }
+       setUser_num("");//사용자 입력값 지우기
+    }
+
+    function handleChange(event){ //HTML 마크업의 변경된 값을 state에 반영
+        setUser_num(event.target.value);
+    }
+
+    return (
+        <div>
+            <h1>숫자맞추기</h1>
+            <p>1~100사이 컴퓨터의 숫자를 맞춰보세요</p>
+            <input type="number" 
+                    min="1" 
+                    max="100"
+                    value={user_num}
+                    onChange={handleChange}/>
+            <button onClick={checkGuess}>정답확인</button>
+            <div>확인결과: {result} </div>
+            <Greeting
+                     isLoggedIn={false}/>
+        </div>
+    );
+}
+
+export default MyNum;
