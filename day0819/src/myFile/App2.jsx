8c3d7dbb8f7d8b8ae63c4f7f166c6299be4a07af
Yewon_Lee <65122491+dldpdnjs21@users.noreply.github.com>
2024-08-19 15:40:45 +0900

    엘리멘트 변수, 인라인 조건, 컴포넌트 렌더링 막기

---
+++ day0819/src/myFile/App2.jsx
@@ -0,0 +1,34 @@
+import "./welcome.css"
+
+function Contacts() {
+    return <div className="Contacts" />;
+  }
+  import "./welcome.css"
+  
+  function Chat() {
+    return <div className="Chat" />;
+  }
+  function SplitPane(props) {
+    return (
+      <div className="SplitPane">
+        <div className="SplitPane-left">
+          {props.left}
+        </div>
+        <div className="SplitPane-right">
+          {props.right}
+        </div>
+      </div>
+    );
+  }
+  function App2(props) {
+    return (
+      <SplitPane
+        left={
+          <Contacts />
+        }
+        right={
+          <Chat />
+        } />
+    );
+  }
+ export default App2;
---
+++ day0819/src/myFile/WelcomeDialog.jsx
@@ -0,0 +1,13 @@
+// WelcomeDialog.jsx
+import FancyBorder from "./Fancyborder";
+import "./welcome.css"
+
+function WelcomeDialog(props){
+    return (
+        <FancyBorder color="blue">
+            <h1 className="Dialog-title">어서오세요</h1>
+            <p className="Dialog-Message">우리 사이트에 방문하신 것을 환영합니다</p>
+        </FancyBorder>
+    )
+}
+export default WelcomeDialog;
