39d41cd4b4af7d2d2c967aefd6d977097d0a46e3
Yewon_Lee <65122491+dldpdnjs21@users.noreply.github.com>
2024-08-21 16:44:53 +0900

    리액트css : styled-components

--- day0821/src/csstest/ButtonApp.jsx
+++ day0821/src/csstest/ButtonApp.jsx
@@ -1,11 +1,23 @@
-import Button from "./Button1";
-import Box from "./Box1";
+// import Button from "./Button1";
+// import Box from "./Box1";
+
+
+// function ButtonApp(props){
+//     return(
+//         <div className="App">
+//             <Button size='big'></Button>
+//             <Button size='small'></Button>
+//             <Box size='big'></Box>
+//             <Box size='small'></Box>
+//         </div>
+//     );
+// }
+
+import Box from "./Box4";
 
 function ButtonApp(props){
     return(
         <div className="App">
-            <Button size='big'></Button>
-            <Button size='small'></Button>
             <Box size='big'></Box>
             <Box size='small'></Box>
         </div>
