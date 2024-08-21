7a386a764e8994fa78d606c366f2fd7ed968d67d
Yewon_Lee <65122491+dldpdnjs21@users.noreply.github.com>
2024-08-21 16:30:02 +0900

    리액트_css적용

---
+++ day0821/src/csstest/CssExam01.jsx
@@ -0,0 +1,7 @@
+export default function CssExam01(props) {
+    return (
+        <div>
+            <h1 style={{color:"red",border:"1px solid black"}}>인라인CSS</h1>
+        </div>
+    );
+}
---
+++ day0821/src/csstest/CssExam02.jsx
@@ -0,0 +1,9 @@
+import "./exam02.css";
+
+export default function CssExam02(props){
+    return(
+        <div>
+            <h1 className="ureca">인라인CSS</h1>
+        </div>
+    )
+}
